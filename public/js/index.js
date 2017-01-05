403251cedf20bb8a4f0b23b1de6be2368b9d9a3f
root <adhikariratna52@gmail.com>
2017-01-05 18:44:25 +0545

    timistampped using moment

--- public/js/index.js
+++ public/js/index.js
@@ -11,16 +11,18 @@ socket.on('disconnect',function () {
 
 socket.on('newMsg',function (msg) {
   //console.log('got msg: ',msg);
+  var fmtTime = moment(msg.createdAt).format('h:mm a');
   var li = jQuery('<li></li>');
-  li.text(`${msg.from}: ${msg.text}`);
+  li.text(`${msg.from} ${fmtTime}: ${msg.text}`);
   jQuery('#msgs').append(li);
 });
 
 socket.on('newLocMsg',function (msg) {
+  var fmtTime = moment(msg.createdAt).format('h:mm a');
   var li = jQuery('<li></li>');
   var a = jQuery('<a target="_blank">View User Location</a>');
 
-  li.text(`${msg.from}: `);
+  li.text(`${msg.from} ${fmtTime}: `);
   a.attr('href',msg.url);
   li.append(a);
   jQuery('#msgs').append(li);
@@ -34,20 +36,22 @@ jQuery('#msg-form').on('submit', function (e) {
   },function (data){
     console.log(data);
   });
-  jQuery('#msg').val('');
+  jQuery('#msg').val(''); //clearing the textbox
 });
 
 var locBtn = jQuery('#send-loc');
 locBtn.on('click',function () {
   if(!navigator.geolocation) return alert('No support for geolocation');
-
+  locBtn.attr('disabled','disabled').text('Sending Location...');
   navigator.geolocation.getCurrentPosition(function (pos) {
     // console.log(pos);
+    locBtn.removeAttr('disabled').text('Send Location');
     socket.emit('createLocMsg',{
       lt:pos.coords.latitude,
       lg:pos.coords.longitude
     });
   },function () {
+    locBtn.removeAttr('disabled').text('Send Location');
     alert('Unable to fetch ur location');
   });
 });
